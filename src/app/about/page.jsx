1568e372185e2998675a78a9abfb70ecce014e2f
mehedi66 <mh5203280@gmail.com>
2025-11-23 15:55:20 +0600

    featch section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,18 @@
-export default function about () {
-     return (
-          <div>
-               <h1>This is About Page</h1>
-          </div>
-     )
+import Link from "next/link";
+
+export default async function about() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await data.json();
+  console.log(posts);
+  
+  return (
+    <div>
+      <h1>This is About Page</h1>
+      <div className="flex flex-col">
+          {
+               posts.map(post => <Link key={post.id} href={`/products/${post.id}`}>{post.name}</Link>)
+          }
+      </div>
+    </div>
+  );
 }
--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,8 +1,12 @@
-export default async function products ({params}){
-     const { id } = await params
-     return(
-          <div>
-               <h1>Product details:{id}</h1>
-          </div>
-     )
+export default async function products({ params }) {
+  const { id } = await params;
+  const data = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const posts = await data.json();
+  console.log(posts);
+
+  return (
+    <div>
+      <h1>Product details:{posts.name}</h1>
+    </div>
+  );
 }
